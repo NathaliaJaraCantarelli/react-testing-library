0826555347b90c989f683edcb0e4a034af455e1d
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-10-19 16:41:41 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import About from '../pages/About';
+
+describe('Testa o componente About', () => {
+  test('Se o componente About contém um h2 com o texto About Pokédex', () => {
+    render(<About />);
+    const heading = screen.getByRole('heading', { level: 2, name: /About Pokédex/ });
+    expect(heading).toBeInTheDocument();
+  });
+  test('Se o componente About contém uma imagem e é o src especifico', () => {
+    render(<About />);
+    const image = screen.getByRole('img');
+    const srcImage = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(image.src).toBe(srcImage);
+  });
+});
